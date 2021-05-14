8add3c1a843d03e32336d8f592bf8c7f50098c2c
Katerina964 <katrin.balakina@gmail.com>
2021-05-14 21:16:16 +0300

    css again

--- django/canban_board/models.py
+++ django/canban_board/models.py
@@ -9,10 +9,10 @@ class Card(models.Model):
     STATE = [(TODO, 'TODO'),
              (DOING, 'DOING'),
              (DONE, 'DONE')]
-    name = models.CharField(max_length=150)
-    description = models.TextField()
-    start_date = models.DateField()
-    end_date = models.DateField()
+    name = models.CharField(max_length=150, blank=True)
+    description = models.TextField(blank=True)
+    start_date = models.DateField(blank=True)
+    end_date = models.DateField(blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     state = models.CharField(max_length=2,
                              choices=STATE,
